06e650304ee8fb4e6fe1ee9fda9c5c8309225f6c
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2023-12-07 18:28:28 +0800

    fix: title

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Tailwind CSS Page</title>
+    <title>iSafe</title>
     <!-- Add the link to the Tailwind CSS CDN -->
     <link rel="stylesheet" href="css/style.css">
 </head>
