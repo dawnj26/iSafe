2c29a393e6177837694cd3f55f0d99c26e7bf9f5
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2023-12-08 11:47:58 +0800

    add: script for opening and closing modal

--- public/js/close_modal.js
+++ public/js/close_modal.js
@@ -1,11 +1,22 @@
-const modal = document.querySelector('#notification-modal')
-const openModal = document.querySelector('#open-modal')
-const closeModal = document.querySelector('#close-modal')
+const notifModal = document.querySelector('#notification-modal')
+const appointmentModal = document.querySelector('#appointment-modal')
+const openNotification = document.querySelector('#open-notification')
+const openAppointment = document.querySelector('#open-appointment')
+const closeAppointment = document.querySelector('#close-appointment')
+const closeNotification = document.querySelector('#close-notification')
 
-openModal.addEventListener('click', () => {
-    modal.classList.replace('hidden', 'grid')
+openNotification.addEventListener('click', () => {
+    notifModal.classList.replace('hidden', 'grid')
 })
 
-closeModal.addEventListener('click', () => {
-    modal.classList.replace('grid', 'hidden')
+closeNotification.addEventListener('click', () => {
+    notifModal.classList.replace('grid', 'hidden')
+})
+
+openAppointment.addEventListener('click', () => {
+    appointmentModal.classList.replace('hidden', 'grid')
+})
+
+closeAppointment.addEventListener('click', () => {
+    appointmentModal.classList.replace('grid', 'hidden')
 })
