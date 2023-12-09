ffae2daa4e76b72d5005d36b8335fd166714cf4c
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2023-12-09 16:25:29 +0800

    add: php script for getting all the users except the user

--- src/new_message.php
+++ src/new_message.php
@@ -1,1 +1,21 @@
 <?php
+
+require "../config/config.php";
+
+$id = "21-UR-0001";
+
+$result = $mainConn->query("SELECT user_id, first_name, last_name FROM user WHERE user_id != '$id'");
+$count = 0;
+$json = array();
+if ($result->num_rows > 0) {
+    while ($data = $result->fetch_assoc()) {
+
+        $initials = strtoupper(substr($data['first_name'], 0, 1)) . strtoupper(substr($data['last_name'], 0, 1));
+        $full_name = $data['first_name'] . ' '  . $data['last_name'];
+        $json[] = array('user_id'=>$data['user_id'], 'full_name'=>$full_name, 'initials'=>$initials);
+
+
+    }
+
+}
+echo json_encode($json);
