c814f9b1ff559459bf9e78c7cd7da8b87f18573c
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2023-12-11 19:43:40 +0800

    add: account authentication

--- src/login-reg/auth.php
+++ src/login-reg/auth.php
@@ -1,1 +1,16 @@
 <?php
+if (empty($_POST['id'])) {
+    die();
+}
+
+require "../../config/config.php";
+
+$id = $_POST['id'];
+$valid = check_id_login($id);
+
+if (!empty($valid)) {
+    echo $valid;
+    return;
+}
+
+
