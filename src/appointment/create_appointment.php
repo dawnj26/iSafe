d3d7e1040f7cb2b2e3d6c7cc27f80664ab1d8910
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2023-12-13 21:59:01 +0800

    add: database logic for creating appointment

--- src/appointment/create_appointment.php
+++ src/appointment/create_appointment.php
@@ -1,1 +1,30 @@
 <?php
+if (empty($_POST['violence']) || empty($_POST['description']) || empty($_POST['dateOfEvent']) || empty($_POST['timeOfEvent']) || empty($_POST['counselor']) || empty($_POST['dateOfAppointment']) || empty($_POST['timeOfAppointment']) || empty($_POST['latitude']) || empty($_POST['longitude'])) {
+    exit('Please fill in all fields');
+}
+
+//session_start();
+//if (!isset($_SESSION['user_id'])) {
+//    exit('Please login');
+//}
+
+$id = '21-UR-0001';
+$violence = $_POST['violence'];
+$description = $_POST['description'];
+$dateOfEvent = $_POST['dateOfEvent'];
+$timeOfEvent = $_POST['timeOfEvent'];
+$counselor = $_POST['counselor'];
+$dateOfAppointment = $_POST['dateOfAppointment'];
+$timeOfAppointment = $_POST['timeOfAppointment'];
+$latitude = $_POST['latitude'];
+$longitude = $_POST['longitude'];
+
+require '../../config/config.php';
+
+$q = "INSERT INTO appointment (creator_id, counselor_id, report_title, report_desc, time_of_event, date_of_event,status,map_longitude, map_latitude, appointment_time, appointment_date) VALUES ('$id', '$counselor', '$violence', '$description', '$timeOfEvent', STR_TO_DATE('$dateOfEvent', '%m/%d/%Y'), 'unfinished', '$longitude', '$latitude', '$timeOfAppointment', STR_TO_DATE('$dateOfAppointment', '%m/%d/%Y'))";
+
+if (!insert_data($q)) {
+    exit("Query error");
+}
+
+echo "SUCCESS";
--- src/appointment/get_counselor_sched.php
+++ src/appointment/get_counselor_sched.php
@@ -1,1 +1,20 @@
 <?php
+if (empty($_POST['counselor_id'])) {
+    exit('Please fill in all fields');
+}
+
+require '../../config/config.php';
+
+$id = $_POST['counselor_id'];
+
+$q = "SELECT day_of_week FROM couselor_schedule WHERE couselor_id = '$id'";
+$result = $mainConn->query($q);
+$days = array();
+if ($result->num_rows > 0) {
+
+    while ($row = $result->fetch_assoc()) {
+        $days[] = $row['day_of_week'];
+    }
+
+}
+echo json_encode($days);
