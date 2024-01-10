113d0f241d59e365f5f92ab684a883994271e4c5
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-01-10 17:24:16 +0800

    add: user session

--- src/chat/get_room.php
+++ src/chat/get_room.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-if (empty($_POST['receiver']) || empty($_SESSION['id'])) {
+if (empty($_POST['receiver']) || !isset($_SESSION['id'])) {
     exit();
 }
 
@@ -13,12 +13,13 @@ $receiver = $_POST['receiver'];
 $result = $mainConn->query("SELECT room_id FROM 100ms_rooms WHERE counselor_id = '$sender'");
 $data = $result->fetch_assoc();
 
+
 $codes = get_room_code($data['room_id']);
+
 $room_patient = 'https://dawnj26-videoconf-1956.app.100ms.live/meeting/'.$codes['patient'];
 $room_host = 'https://dawnj26-videoconf-1956.app.100ms.live/meeting/'.$codes['host'];
 
 insert_data("INSERT INTO chat (sender_id, receiver_id, text_message) VALUES ('$sender', '$receiver', '$room_patient')");
-
 $links = [$room_host, $room_patient];
 
 echo json_encode($links);
