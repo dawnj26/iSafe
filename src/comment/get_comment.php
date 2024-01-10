70f4e838a92f72891cfa1ed84e917843ed96fea0
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-01-10 17:24:42 +0800

    update: query to get user_id

--- src/comment/get_comment.php
+++ src/comment/get_comment.php
@@ -8,7 +8,7 @@ $post_id = $_POST['post_id'];
 
 require '../../config/config.php';
 
-$query = "SELECT user.first_name, user.last_name, post_comments.comment_text, DATE_FORMAT(post_comments.comment_date_created, '%M %e, %Y') AS formatted_date FROM post_comments INNER JOIN user ON post_comments.user_id = user.user_id WHERE post_id = $post_id";
+$query = "SELECT user.user_id, user.first_name, user.last_name, post_comments.comment_text, DATE_FORMAT(post_comments.comment_date_created, '%M %e, %Y') AS formatted_date FROM post_comments INNER JOIN user ON post_comments.user_id = user.user_id WHERE post_id = $post_id";
 $result = $mainConn->query($query);
 
 $data = [];
@@ -16,6 +16,7 @@ $data = [];
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $row_data = [
+            'user_id' => $row['user_id'],
             'first_name' => $row['first_name'],
             'last_name' => $row['last_name'],
             'comment' => $row['comment_text'],
